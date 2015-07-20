83aa1d811ffbd4668e9c409faa6e88a34e08cdfa
rastating <rob@rastating.com>
2015-07-20 02:59:49 +0100

    Add categorised print helper method

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+gem 'colorize'
 gem 'nokogiri'
 gem 'slop'
 gem 'typhoeus'
--- joomlavs.rb
+++ joomlavs.rb
@@ -1,4 +1,5 @@
 require 'slop'
+require 'colorize'
 require_relative 'lib/component_scanner'
 
 def print_banner
@@ -16,12 +17,28 @@ def print_banner
                                                            Version 0.1
 
 ----------------------------------------------------------------------
-)
+
+).light_white
+end
+
+def print_line(type, text)
+  if type == :good
+    print '[+] '.green
+  elsif type == :warning
+    print '[!] '.yellow
+  elsif type == :info
+    print '[i] '.cyan
+  elsif type == :error
+    print '[!] '.red
+  else
+    print '    '
+  end
+      
+  print "#{text}\r\n".light_white
 end
 
 def main
   print_banner
-  print "\r\n"
 
   opts = Slop.parse do |o|
     o.string '-u', '--url', 'The Joomla URL/domain to scan.'
@@ -29,7 +46,8 @@ def main
 
   if opts[:url]
     c = ComponentScanner.new(opts[:url])
-    puts "Scanning #{c.target_uri}...\r\n"
+    print_line(:good, "URL: #{c.target_uri}")
+    print_line(:good, "Started: #{Time.now.asctime}")
     c.scan
   else
     puts opts
