a3cac5d2078b207c447ada287f71b59f751dc469
rastating <rob@rastating.com>
2015-07-26 14:41:13 +0100

    Add max threads option

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -5,7 +5,7 @@ class Scanner
   def initialize(target_uri, opts)
     update_target_uri target_uri
     @opts = opts
-    @hydra = Typhoeus::Hydra.hydra
+    @hydra = Typhoeus::Hydra.new(max_concurrency: opts[:threads])
   end
 
   def target_uri
