b192b861da3ea95a1a568e490e80cfe1cb10f8ab
rastating <rob@rastating.com>
2015-08-01 18:28:11 +0100

    Add Joomla! vulnerability scanning

--- joomlavs.rb
+++ joomlavs.rb
@@ -5,6 +5,17 @@ require_relative 'lib/component_scanner'
 require_relative 'lib/module_scanner'
 require_relative 'lib/fingerprint_scanner'
 
+def display_vulns(vulns, output)
+  vulns.each do |v|
+    output.print_line_break
+    output.print_line(:error, "Title: #{v['title']}")
+    output.print_indent("Reference: https://www.exploit-db.com/exploits/#{v['edbid']}") if v['edbid']
+    output.print_indent("Reference: http://osvdb.org/#{v['osvdbid']}") if v['osvdbid']
+    output.print_line(:info, "Fixed in: #{v['fixed_in']}") if v['fixed_in']
+    output.print_line_break
+  end
+end
+
 def display_detected_extension(e, output)
   output.print_line_break
   output.print_good("Name: #{e[:name]} - v#{e[:version]}")
@@ -14,16 +25,25 @@ def display_detected_extension(e, output)
   output.print_indent("Author: #{e[:author]}") unless e[:author].empty?
   output.print_indent("Author URL: #{e[:author_url]}") unless e[:author_url].empty?
   
-  e[:vulns].each do |v|
-    output.print_line_break
-    output.print_line(:error, "Title: #{v['title']}")
-    output.print_indent("Reference: https://www.exploit-db.com/exploits/#{v['edbid']}") if v['edbid']
-    output.print_indent("Reference: http://osvdb.org/#{v['osvdbid']}") if v['osvdbid']
-    output.print_line(:info, "Fixed in: #{v['fixed_in']}") if v['fixed_in']
-    output.print_line_break
-  end
+  display_vulns(e[:vulns], output)
 
   output.print_horizontal_rule(:default)
+end
+
+def joomla_vulnerabilities(version)
+  json = File.read('data/joomla.json')
+  vulns = JSON.parse(json)
+  found = Array.new
+
+  vulns.each do |v|
+    if v['introduced_in'].nil? or Gem::Version.new(v['introduced_in']) <= version
+      if v['fixed_in'].nil? or Gem::Version.new(v['fixed_in']) > version
+        found.push(v)
+      end
+    end
+  end
+
+  found
 end
 
 def check_target_redirection(scanner, output, opts)
@@ -81,12 +101,6 @@ def main
     target = scanner.target_uri
 
     output.print_line_break
-    output.print_good("Determining Joomla version...") if opts[:verbose]
-    version = scanner.version_from_readme
-    output.print_good("Joomla version #{version} identified from README.txt") if version
-    output.print_error("Couldn't determine version from README.txt") unless version
-
-    output.print_line_break if opts[:verbose]
     output.print_good('Checking if registration is enabled...') if opts[:verbose]
     output.print_warning("Registration is enabled: #{scanner.target_uri}#{scanner.registration_uri}") if scanner.user_registration_enabled
     output.print_good('User registration is not enabled.') if !scanner.user_registration_enabled && opts[:verbose]
@@ -105,6 +119,21 @@ def main
     output.print_warning("Components listing enabled: #{scanner.target_uri}/components") if scanner.components_listing_enabled
     output.print_warning("Modules listing enabled: #{scanner.target_uri}/administrator/modules") if scanner.administrator_modules_listing_enabled
     output.print_warning("Modules listing enabled: #{scanner.target_uri}/modules") if scanner.modules_listing_enabled
+
+    output.print_line_break
+    output.print_good("Determining Joomla version...") if opts[:verbose]
+    version = scanner.version_from_readme
+    version = 3.0
+    output.print_good("Joomla version #{version} identified from README.txt") if version
+    output.print_error("Couldn't determine version from README.txt") unless version
+
+    if version
+      joomla_vulns = joomla_vulnerabilities(Gem::Version.new(version))
+      if joomla_vulns
+        output.print_warning("Found #{joomla_vulns.length} vulnerabilities affecting this version of Joomla!")
+        display_vulns(joomla_vulns, output)
+      end
+    end
 
     if opts[:scan_all] || opts[:scan_components]
       scanner = ComponentScanner.new(target, opts)
